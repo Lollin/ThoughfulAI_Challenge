8d89c4fe9c8acb3810484f6de31d870ffd047918
Danilo Menezes <danilo_kurama@hotmail.com>
2024-06-26 23:05:52 -0300

    Final Project

--- tasks.py
+++ tasks.py
@@ -3,13 +3,9 @@ from robocorp.tasks import task
 from RPA.Excel.Files import Files
 from RPA.HTTP import HTTP
 from robocorp import workitems
-import json
 from datetime import datetime, timedelta
 import dateparser
-
-# from pathlib import Path
-# import os
-# import requests
+import os
 import re
 
 # FILE_NAME = "challenge.xlsx"
@@ -23,18 +19,20 @@ http = HTTP()
 
 # Load configurations
 
-search_phrase = workitems["search_phrase"]
-news_category = workitems["news_category"]
-if workitems["months"] == 0:
-    months = 1
-else:
-    months = workitems["months"]
+for item in workitems.inputs:
+    search_phrase = item.payload["search_phrase"]
+    print("Search: " + search_phrase)
+    news_category = item.payload["news_category"]
+    print("Category: " + news_category)
+    if item.payload["months"] == 0:
+        months = 1
+    else:
+        months = item.payload["months"]
+end_date = datetime.now()
+print("Month: " + str(months))
+
 
 # Define the date range for the news articles
-end_date = datetime.now()
-start_date = end_date - timedelta(days=30 * months)
-
-
 def is_date_within_range(date_text):
     try:
         data_interprated = dateparser.parse(date_text)
@@ -68,6 +66,7 @@ def main():
     """
     Solve the RPA challenge
     """
+
     print("Opening browser")
     browser.open_browser("https://www.latimes.com/", browser="firefox")
     browser.maximize_browser_window()
@@ -97,9 +96,7 @@ def main():
     for i, article in enumerate(articles):
         try:
             value = i + 1
-            content = browser.find_element(
-                locator='(//div[contains(@class, "promo-content")])[{}]'.format(value)
-            )
+            print("Reading the News")
             title = browser.find_element(
                 locator='(//h3[contains(@class, "promo-title")])[{}]'.format(value)
             ).text
@@ -122,9 +119,14 @@ def main():
                 )
             )
             image_url = image.get_attribute("srcset").split(" ")[0]
-            print(image_url)
             image_filename = image_url.split(sep="-", maxsplit=2)[2]
-            print(image_filename)
+            name_base, extension = os.path.splitext(image_filename)
+            print(name_base)
+            print(extension)
+            if not extension or len(extension) > 4:
+                print("Apply extension .jpg")
+                image_filename = image_filename + ".jpg"
+
             http.download(image_url, f"output/{image_filename}")
 
             money_mentioned = contains_money(title) or contains_money(description)
@@ -154,10 +156,12 @@ def main():
         "Count Search Phrase",
         "Money",
     ]
+
     for col, header in enumerate(headers, start=1):
-        excel.set_worksheet_value(
-            1, col, header.upper(), bold=True, horizontal_alignment="center"
-        )
+        excel.set_worksheet_value(1, col, header.capitalize())
+
+    excel.set_styles("A1:F1", bold=True, font_name="Arial", size=12)
+    excel.set_styles("A2:F12", font_name="Arial", size=10)
 
     for i, result in enumerate(results, start=2):
         excel.set_worksheet_value(i, 1, result["title"])
